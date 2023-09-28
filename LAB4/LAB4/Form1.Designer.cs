405098178ea25cbfeb18164b9e4a5c15290bfd62
MMH804 <131004371+MMH804@users.noreply.github.com>
2023-09-28 14:32:01 +0500

    Add files via upload

---
+++ LAB4/LAB4/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace LAB4
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBoxInput = new TextBox();
+            button1 = new Button();
+            textBox2 = new TextBox();
+            SuspendLayout();
+            // 
+            // textBoxInput
+            // 
+            textBoxInput.Location = new Point(55, 186);
+            textBoxInput.Multiline = true;
+            textBoxInput.Name = "textBoxInput";
+            textBoxInput.Size = new Size(164, 102);
+            textBoxInput.TabIndex = 0;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(298, 228);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 1;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(569, 186);
+            textBox2.Multiline = true;
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(164, 102);
+            textBox2.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(textBox2);
+            Controls.Add(button1);
+            Controls.Add(textBoxInput);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBoxInput;
+        private Button button1;
+        private TextBox textBox2;
+    }
+}
